0696cb1b517a7d05e66c3aeaed05df1d7506bd99
sjlee <thetomatoaddict@gmail.com>
2022-11-04 21:41:09 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by sujin on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
